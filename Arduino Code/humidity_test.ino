dfb672beb0a5dcf6b1c1813a8ebc52af47d07e8b
Bilal Mahmood <bilal.mahmood1706@gmail.com>
2024-04-12 15:14:13 -0400

    Replaced old test file
    Replaced the initial test file with a new version that can be logged into excel with the PLX DAQ

--- Arduino Code/humidity_test.ino
+++ Arduino Code/humidity_test.ino
@@ -1,3 +1,5 @@
+#include <ArduinoLowPower.h>
+
 // Basic demo for reading Humidity and Temperature
 #include <Wire.h>
 #include <Adafruit_HTS221.h>
@@ -9,18 +11,21 @@
 #define HTS_SCK 13
 #define HTS_MISO 12
 #define HTS_MOSI 11
+float farenheit_temp;
 
 Adafruit_HTS221 hts;
 void setup(void) {
   Serial.begin(115200);
+  Serial.println("CLEARSHEET");
+  Serial.println("LABEL,Time,Humidity (rH),Temperature (F)");
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("Adafruit HTS221 test!");
 
   // Try to initialize!
-  if (!hts.begin_I2C()) {
-//  if (!hts.begin_SPI(HTS_CS)) {
-//  if (!hts.begin_SPI(HTS_CS, HTS_SCK, HTS_MISO, HTS_MOSI)) {
+  //if (!hts.begin_I2C()) {
+  if (!hts.begin_SPI(HTS_CS)) {
+//if (!hts.begin_SPI(HTS_CS, HTS_SCK, HTS_MISO, HTS_MOSI)) {
     Serial.println("Failed to find HTS221 chip");
     while (1) { delay(10); }
   }
@@ -42,8 +47,11 @@ void loop() {
   sensors_event_t temp;
   sensors_event_t humidity;
   hts.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+  farenheit_temp = 32+((float)temp.temperature * (float)9/5);
+  Serial.print("DATA,TIME,");
+  Serial.print(humidity.relative_humidity);
+  Serial.print(",");
+  Serial.println(farenheit_temp);
 
-  delay(500);
+  LowPower.deepSleep(10000);
 }
